d2622cca05c6ff69c0733ced4937d675bcf47cdd
Akhil Devarasetty <akhild2004@gmail.com>
2023-10-07 14:58:43 -0700

    I dont know what happened

--- src/Components/Body/index.js
+++ src/Components/Body/index.js
@@ -1,23 +1,9 @@
 import BasicCard from '../Card1'
-import Stack from '@mui/material/Stack';
-import Paper from '@mui/material/Paper';
-import { styled } from '@mui/material/styles';
 
 export default function NavBar() {
     return (
         <div className="App-body"> 
-        <Stack direction="row" spacing={2}>
-            <DemoPaper>
-                <BasicCard/>
-            </DemoPaper>
-        </Stack>
+        <BasicCard/>
         </div>
     );
 }
-
-const DemoPaper = styled(Paper)(({ theme }) => ({
-    padding: theme.spacing(2),
-    ...theme.typography.body2,
-    textAlign: 'center',
-    backgroundColor: 'transparent'
-  }));
