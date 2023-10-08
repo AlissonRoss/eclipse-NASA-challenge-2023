26ae88729a545011125f8d0164eb21cad453cd5a
AlissonRoss <alisson@alisson.me>
2023-10-08 08:21:26 -0700

    Add solar/lunar eclipse labels

--- src/Components/Eclipse/index.js
+++ src/Components/Eclipse/index.js
@@ -4,8 +4,10 @@ export default function Eclipse() {
   return (
     <div className='header-spacer' id="eclipse">
       <div class='eclipse-animation'>
-        <div className='scale'>Solar & Lunar Eclipse<br/>(to scale)</div>
-        <div className='Sun-Label'>⬆️ Sun<br/>387x further</div>
+        <div className='scale'>To Scale</div>
+        <div className='solar-eclipse'>Solar Eclipse</div>
+        <div className='lunar-eclipse'>Lunar Eclipse</div>
+        <div className='Sun-label'>⬆️ Sun<br/>387x further</div>
         <div className='Earth'>
           <div className='Moon-Orbit'>
             <div className='Moon'>
