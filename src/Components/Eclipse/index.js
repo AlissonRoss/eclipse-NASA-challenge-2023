cf6fd4e0dd2595400cd684ec35c3e1e026b12d12
AlissonRoss <alisson@alisson.me>
2023-10-08 08:14:53 -0700

    Shrunk and labeled model

--- src/Components/Eclipse/index.js
+++ src/Components/Eclipse/index.js
@@ -4,8 +4,8 @@ export default function Eclipse() {
   return (
     <div className='header-spacer' id="eclipse">
       <div class='eclipse-animation'>
-        <div className='scale'>Eclipse<br/>(to scale)</div>
-        <div className='Sun'>Sun<br/>387x further</div>
+        <div className='scale'>Solar & Lunar Eclipse<br/>(to scale)</div>
+        <div className='Sun-Label'>⬆️ Sun<br/>387x further</div>
         <div className='Earth'>
           <div className='Moon-Orbit'>
             <div className='Moon'>
@@ -14,17 +14,6 @@ export default function Eclipse() {
           </div>
           <div className='Shadow'></div>
         </div>
-        <button onClick={
-          function(e){
-            e.currentTarget.parentElement.classList.toggle('lunar-eclipse');
-
-            // Restart animation cycles
-            e.currentTarget.parentElement.classList.remove('eclipse-animation');
-            setTimeout((elem) => {
-              elem.classList.add('eclipse-animation');
-            }, 1, e.currentTarget.parentElement);
-          }
-        }>Switch</button>
       </div>
     </div>
   );
